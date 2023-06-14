fcc0ca23ae9fd99b6fa8642246e58f16e9f14d7b
Javed Iqbal <javed.iqbal@invobyte.com>
2023-06-14 11:27:14 +0500

    ecommerce 201 is completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
+import { ProductListComponent } from './components/product-list/product-list.component';
+import { HttpClientModule } from '@angular/common/http'
+import { ProductService } from './services/product.service';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    ProductListComponent,
+     ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [ProductService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/services/product.service.ts
@@ -0,0 +1,30 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Product } from '../common/product';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  private baseUrl = 'http://localhost:8080/api/products';
+
+  constructor(private httpClint: HttpClient) { }
+
+  getProductList(): Observable<Product[]> {
+
+    return this.httpClint.get<GetResponse>(this.baseUrl).pipe(
+      map(response => response._embedded.products)
+    
+    );
+    
+  }
+}
+
+interface GetResponse {
+_embedded: {
+   products: Product[];
+}
+}
